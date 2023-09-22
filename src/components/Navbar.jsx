70c738ebee6dce58591a744173d5d99ee3ee6d7d
Thulith de Zoysa <139491011+thulithdezoysa@users.noreply.github.com>
2023-09-22 03:18:10 +0100

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import './Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faCartShopping, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faCartShopping, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import Button from './Button'
 import Modal from './Modal.jsx'
 
 export default function Navbar (props) {
+
+    const [menuToggle, setMenuToggle] = useState(false)
+
+        function toggleMenuitems(){
+            setMenuToggle(() => !menuToggle)
+            menuToggle ? 'inherit' : 'none'
+            alert('clicked')
+
+        }
 
         function searchItems(){
             alert('This function is not available at the moment')
@@ -14,8 +24,6 @@ export default function Navbar (props) {
     
     return (
         <nav className='navbar'>
-
-      
 
             <Link to="/"><img src='./assets/logo.png' className='navbar__logo' alt='Kaumadhi logo'/></Link> 
             <div className='navbar__search-bar'>
@@ -28,8 +36,10 @@ export default function Navbar (props) {
                 <FontAwesomeIcon icon={faHeart} className='navbar__icon-seperator' onClick={props.saveModal}/> 
                 <FontAwesomeIcon icon={faCartShopping}  onClick={props.cartModal} className='navbar__cart-icon'/>
             </div>
-            <FontAwesomeIcon icon={faBars} className='navbar__menu-icon' />
-            <ul>
+            <FontAwesomeIcon icon={faBars} className='navbar__menu-icon' onClick={toggleMenuitems} />
+
+            <ul className='navbar__navlinks' style={{display:menuToggle}}>
+            <FontAwesomeIcon icon={faXmark} className='navbar__closeBtn' onClick={toggleMenuitems} />
                 <li>
                     <Link to="/">Home</Link>
                 </li>
